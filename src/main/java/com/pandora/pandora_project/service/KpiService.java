1e195f7e7778318df2706a5182737c73c2e4ff96
Kevin <kevin.w271010@gmail.com>
2025-02-02 22:51:01 +0700

    update download file member and kpi member

--- src/main/java/com/pandora/pandora_project/service/KpiService.java
+++ src/main/java/com/pandora/pandora_project/service/KpiService.java
@@ -66,56 +66,107 @@ public class KpiService{
         this.kpiRepository = kpiRepository;
     }
 
-    public ByteArrayInputStream convertToExcel(List<Member> members) throws IOException {
-        List<Member> memberList = members;
+//    public ByteArrayInputStream convertToExcel(List<Member> members) throws IOException {
+//        List<Member> memberList = members;
+//
+//        Workbook workbook = new XSSFWorkbook();
+//        ByteArrayOutputStream out = new ByteArrayOutputStream();
+//
+//        try {
+//
+//            Sheet sheet = workbook.createSheet();
+//
+//            Row row = sheet.createRow(0);
+//
+//            for (int i = 0; i < KPIMEMBERHEADERS.length; i++) {
+//                Cell cell = row.createCell(i);
+//                cell.setCellValue(KPIMEMBERHEADERS[i]);
+//            }
+//
+//            int rowIndex = 1;
+//
+//            for(Member member: memberList){
+//                Row dataRow = sheet.createRow(rowIndex);
+//                rowIndex++;
+//                dataRow.createCell(0).setCellValue(member.getName());
+//                dataRow.createCell(1).setCellValue(member.getUdomain());
+//                dataRow.createCell(2).setCellValue(member.getKpi().getFinal_score());
+//                int index = 2;
+//                for(KQuarter kQuarter: member.getKpi().getKquarters()){
+//                    dataRow.createCell(++index).setCellValue(kQuarter.getPeriod());
+//                    dataRow.createCell(++index).setCellValue(kQuarter.getTarget());
+//                    dataRow.createCell(++index).setCellValue(kQuarter.getDone());
+//                    dataRow.createCell(++index).setCellValue(kQuarter.getCust_focus());
+//                    dataRow.createCell(++index).setCellValue(kQuarter.getIntegrity());
+//                    dataRow.createCell(++index).setCellValue(kQuarter.getTeamwork());
+//                    dataRow.createCell(++index).setCellValue(kQuarter.getCpoe());
+//                    dataRow.createCell(++index).setCellValue(kQuarter.getOn_schedule());
+//                    dataRow.createCell(++index).setCellValue(kQuarter.getLate());
+//                }
+//            }
+//
+//            workbook.write(out);
+//
+//            return new ByteArrayInputStream(out.toByteArray());
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            System.out.println("fail to import data product excel");
+//            return null;
+//        } finally {
+//            workbook.close();
+//            out.close();
+//        }
+//    }
+    public ByteArrayInputStream convertToExcel(List<Member> memberKPIs) throws IOException {
+        try (Workbook workbook = new XSSFWorkbook();
+             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
 
-        Workbook workbook = new XSSFWorkbook();
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+            Sheet sheet = workbook.createSheet("Member KPI");
 
-        try {
-
-            Sheet sheet = workbook.createSheet();
-
-            Row row = sheet.createRow(0);
-
-            for (int i = 0; i < KPIMEMBERHEADERS.length; i++) {
-                Cell cell = row.createCell(i);
-                cell.setCellValue(KPIMEMBERHEADERS[i]);
+            // Create header row
+            Row headerRow = sheet.createRow(0);
+            String[] headers = {"No", "Name", "Udomain", "Product", "Kpi Product", "Quarter", "Target", "Done", "Ach",
+                    "Cust Focus", "Integrity", "Teamwork", "CPOE", "Average", "On Schedule", "Late", "KPI Final"};
+            for (int i = 0; i < headers.length; i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(headers[i]);
             }
 
+            int memberCount=0;
             int rowIndex = 1;
 
-            for(Member member: memberList){
-                Row dataRow = sheet.createRow(rowIndex);
-                rowIndex++;
-                dataRow.createCell(0).setCellValue(member.getName());
-                dataRow.createCell(1).setCellValue(member.getUdomain());
-                dataRow.createCell(2).setCellValue(member.getKpi().getFinal_score());
-                int index = 2;
-                for(KQuarter kQuarter: member.getKpi().getKquarters()){
-                    dataRow.createCell(++index).setCellValue(kQuarter.getPeriod());
-                    dataRow.createCell(++index).setCellValue(kQuarter.getTarget());
-                    dataRow.createCell(++index).setCellValue(kQuarter.getDone());
-                    dataRow.createCell(++index).setCellValue(kQuarter.getCust_focus());
-                    dataRow.createCell(++index).setCellValue(kQuarter.getIntegrity());
-                    dataRow.createCell(++index).setCellValue(kQuarter.getTeamwork());
-                    dataRow.createCell(++index).setCellValue(kQuarter.getCpoe());
-                    dataRow.createCell(++index).setCellValue(kQuarter.getOn_schedule());
-                    dataRow.createCell(++index).setCellValue(kQuarter.getLate());
+            for (Member memberKPI : memberKPIs) {
+                boolean isFirstQuarter = true;
+                for (KQuarter kQuarter : memberKPI.getKpi().getKquarters()) {
+                    Row row = sheet.createRow(rowIndex++);
+
+                    if (isFirstQuarter) {
+                        row.createCell(0).setCellValue(++memberCount);
+                        row.createCell(1).setCellValue(memberKPI.getName());
+                        row.createCell(2).setCellValue(memberKPI.getUdomain());
+                        row.createCell(3).setCellValue(memberKPI.getProductowner().getProduct().getName());
+                        row.createCell(4).setCellValue(memberKPI.getProductowner().getProduct().getKpi_product_score());
+                        row.createCell(16).setCellValue(5); // kpi final
+                        isFirstQuarter = false;
+                    }
+
+                    row.createCell(5).setCellValue(kQuarter.getPeriod());
+                    row.createCell(6).setCellValue(kQuarter.getTarget());
+                    row.createCell(7).setCellValue(kQuarter.getDone());
+                    row.createCell(8).setCellValue(5); // ach
+                    row.createCell(9).setCellValue(kQuarter.getCust_focus());
+                    row.createCell(10).setCellValue(kQuarter.getIntegrity());
+                    row.createCell(11).setCellValue(kQuarter.getTeamwork());
+                    row.createCell(12).setCellValue(kQuarter.getCpoe());
+                    row.createCell(13).setCellValue(5); // avg
+                    row.createCell(14).setCellValue(kQuarter.getOn_schedule());
+                    row.createCell(15).setCellValue(kQuarter.getLate());
                 }
             }
 
             workbook.write(out);
-
             return new ByteArrayInputStream(out.toByteArray());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("fail to import data product excel");
-            return null;
-        } finally {
-            workbook.close();
-            out.close();
         }
     }
 }
--- src/main/java/com/pandora/pandora_project/service/MemberService.java
+++ src/main/java/com/pandora/pandora_project/service/MemberService.java
@@ -1,10 +1,7 @@
 package com.pandora.pandora_project.service;
 
 import com.pandora.pandora_project.dto.RatedMember;
-import com.pandora.pandora_project.model.KPI;
-import com.pandora.pandora_project.model.KQuarter;
-import com.pandora.pandora_project.model.Member;
-import com.pandora.pandora_project.model.ProductOwner;
+import com.pandora.pandora_project.model.*;
 import com.pandora.pandora_project.repository.MemberRepository;
 import com.pandora.pandora_project.repository.ProductOwnerRepository;
 import jakarta.transaction.Transactional;
@@ -19,6 +16,7 @@ import org.springframework.stereotype.Service;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @Service
@@ -124,48 +122,105 @@ public class MemberService{
         return false;
     }
 
+//    public ByteArrayInputStream convertToExcel(List<Member> members) throws IOException {
+//        List<Member> memberList = members;
+//
+//        Workbook workbook = new XSSFWorkbook();
+//        ByteArrayOutputStream out = new ByteArrayOutputStream();
+//
+//        try {
+//
+//            Sheet sheet = workbook.createSheet();
+//
+//            Row row = sheet.createRow(0);
+//
+//            for (int i = 0; i < MEMBERHEADERS.length; i++) {
+//                Cell cell = row.createCell(i);
+//                cell.setCellValue(MEMBERHEADERS[i]);
+//            }
+//
+//            int rowIndex = 1;
+//
+//            for(Member member: memberList){
+//                Row dataRow = sheet.createRow(rowIndex);
+//
+//                rowIndex++;
+//                dataRow.createCell(0).setCellValue(member.getName());
+//                dataRow.createCell(1).setCellValue(member.getUdomain());
+//                dataRow.createCell(2).setCellValue(member.getDivision());
+//                dataRow.createCell(3).setCellValue(member.getEmail());
+//                dataRow.createCell(4).setCellValue(member.getBiro());
+//                dataRow.createCell(5).setCellValue(member.getEselon_tier());
+//            }
+//
+//            workbook.write(out);
+//
+//            return new ByteArrayInputStream(out.toByteArray());
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            System.out.println("fail to import data product excel");
+//            return null;
+//        } finally {
+//            workbook.close();
+//            out.close();
+//        }
+//    }
     public ByteArrayInputStream convertToExcel(List<Member> members) throws IOException {
-        List<Member> memberList = members;
+        try (Workbook workbook = new XSSFWorkbook();
+             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
 
-        Workbook workbook = new XSSFWorkbook();
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+            Sheet sheet = workbook.createSheet("Members");
 
-        try {
-
-            Sheet sheet = workbook.createSheet();
-
-            Row row = sheet.createRow(0);
-
-            for (int i = 0; i < MEMBERHEADERS.length; i++) {
-                Cell cell = row.createCell(i);
-                cell.setCellValue(MEMBERHEADERS[i]);
+            // Create header row
+            Row headerRow = sheet.createRow(0);
+            String[] headers = {"No", "Name", "Udomain", "Division", "Email", "Biro", "Eselon Tier",
+                    "No.1", "Code", "Name.1", "Status", "Start Date", "End Date", "Due Date"};
+            for (int i = 0; i < headers.length; i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(headers[i]);
             }
 
+            int memberCount=0;
             int rowIndex = 1;
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
-            for(Member member: memberList){
-                Row dataRow = sheet.createRow(rowIndex);
+            for (Member member : members) {
+                // Fill member details
+                Row memberDetailRow = sheet.createRow(rowIndex++);
+                memberDetailRow.createCell(0).setCellValue(++memberCount);
+                memberDetailRow.createCell(1).setCellValue(member.getName());
+                memberDetailRow.createCell(2).setCellValue(member.getUdomain());
+                memberDetailRow.createCell(3).setCellValue(member.getDivision());
+                memberDetailRow.createCell(4).setCellValue(member.getEmail());
+                memberDetailRow.createCell(5).setCellValue(member.getBiro());
+                memberDetailRow.createCell(6).setCellValue(member.getEselon_tier());
 
-                rowIndex++;
-                dataRow.createCell(0).setCellValue(member.getName());
-                dataRow.createCell(1).setCellValue(member.getUdomain());
-                dataRow.createCell(2).setCellValue(member.getDivision());
-                dataRow.createCell(3).setCellValue(member.getEmail());
-                dataRow.createCell(4).setCellValue(member.getBiro());
-                dataRow.createCell(5).setCellValue(member.getEselon_tier());
+                // Fill Subtasks
+                int subtaskCount = member.getSubtasks().size();
+                if (subtaskCount > 0) {
+                    int maxSubtaskRows = subtaskCount - 1;
+                    for (int i = 0; i <= maxSubtaskRows; i++) {
+                        Subtask subtask = member.getSubtasks().get(i);
+                        Row row;
+                        if (i == 0) {
+                            row = memberDetailRow;
+                        } else {
+                            row = sheet.createRow(rowIndex++);
+                        }
+                        row.createCell(7).setCellValue(i+1);
+                        row.createCell(8).setCellValue(subtask.getCode());
+                        row.createCell(9).setCellValue(subtask.getName());
+                        row.createCell(10).setCellValue(subtask.getStatus());
+                        row.createCell(11).setCellValue(dateFormat.format(subtask.getStart_date()));
+                        row.createCell(12).setCellValue(dateFormat.format(subtask.getEnd_date()));
+                        row.createCell(13).setCellValue(dateFormat.format(subtask.getDue_date()));
+                    }
+                }
             }
 
             workbook.write(out);
-
             return new ByteArrayInputStream(out.toByteArray());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("fail to import data product excel");
-            return null;
-        } finally {
-            workbook.close();
-            out.close();
         }
     }
 }
